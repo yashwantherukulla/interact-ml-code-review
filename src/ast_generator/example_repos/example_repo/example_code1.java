e6e3b15c7c1bdd82ecdcafecf4024074dc8ed216
Mohit Talgotra <talgotram@gmail.com>
2024-09-08 08:17:29 +0530

    removing unnecessary files

--- src/ast_generator/example_repos/example_repo/example_code1.java
+++
@@ -1,7 +0,0 @@
-public class example_code1 {
-// Main method - program execution starts here.
-    public static void main(String[] args) {
-    // Prints "Hello, World!" to the console.
-        System.out.println("Hello, World!");
-    }
-}
