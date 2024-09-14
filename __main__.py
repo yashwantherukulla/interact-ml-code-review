bcf04d36bd2817f10525c7d73f79fb3d64679458
Mohit Talgotra <talgotram@gmail.com>
2024-09-15 02:18:08 +0530

    removing unnecessary code

--- __main__.py
+++
@@ -1,7 +0,0 @@
-import code_review_controller
-
-if __name__ == "__main__":
-    example_body = {
-        'repo_links': ["https://github.com/Mohit-Talgotra/regit.git", "https://github.com/kiwi0fruit/ipynb-py-convert.git"]
-    }
-    print(code_review_controller.codeReview(example_body))
